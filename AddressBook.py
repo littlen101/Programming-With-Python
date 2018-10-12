9074069b478a75379faab12cd5f9ef85b3dc8ea1
TJT <tthomas42@gmail.com>
2018-10-12 09:33:49 -0400

    Code refinements made

--- AddressBook.py
+++ AddressBook.py
@@ -2,6 +2,8 @@
 
 import sys
 
+filename = 'AddressBook.txt'
+
 sWelcomePrompt = ('Welcome to Little Black Book! This program will allow you to add'
 '\nan unlimited number contacts and the associated information for each.'
 '\nIf you do not want to enter anything for a given field, just press ENTER'
@@ -15,14 +17,14 @@ sPhoneType = 'Enter "H" if this is a HOME number or W if this is a WORK number: 
 sEmail = 'Email Address'
 sEmailType = 'Enter "H" if this is a HOME email or W if this is a WORK email: '
 sAddress = 'Physicial Address: '
-sBirthday = 'Brithday: '
+sBirthday = 'Birthday: '
 sNickname = 'Nickname(s): '
 sOccupation = 'Occupation: '
 sCompany = 'Company: '
 sAddMore = ('Your file has been updated.'
-'\nIf you wish to add more contacts, enter "Y".'
-'\nPress "N" to stop and view your contact list.'
-'\nPress ENTER to exit the program: ')
+'\nIf you wish to add more contacts, type "Y" and press ENTER.'
+'\nType "N" and press ENTER to stop and view your contact list.'
+'\nOr, simply press ENTER to exit the program.' + lb)
 lstTypes = ['H','h','W','w']
 
 def homeVSwork(x,sInputVerbiage,sPrompt):
@@ -34,35 +36,63 @@ def homeVSwork(x,sInputVerbiage,sPrompt):
         y = sInputVerbiage + ' (Work)'
     return(y)
 
+def storeInput(filename,enteredValues,cntNumEntries):
+    if cntNumEntries == 1:
+        f = open(filename,'w+')
+    else:
+        f = open(filename,'a')
+    for i in enteredValues:
+        f.write(i)
+    f.close
+
+def getInput(filename,read):
+    fileOutput = open(filename,read)
+    print(fileOutput.read())
+    fileOutput.close()
+
+def reqFld(x,sPrompt):
+    while x == '':
+        print('You must enter a value for this field.')
+        x = input(sPrompt)
+    return x
+
 def Main():
-    input_welcome = input(sWelcomePrompt)
+    input(sWelcomePrompt)
     #Throw this all into a While loop for as long as the user wants to add more entries
     input_sAddMore = 'Y'
     cntAddition = 0
     while input_sAddMore == 'Y' or input_sAddMore == 'y':
         lb
         lb
-        input_sName = input(sName)
+        input_sName = reqFld(input(sName),sName)
         input_sPhone = input(sPhone)
         if input_sPhone != '':
             input_sPhoneType = input(sPhoneType)
             input_sPhone = homeVSwork(input_sPhoneType,input_sPhone,sPhoneType)         
-        cntEmail = 1      
+        cntEmail = 1
+        canEnterAdditionalEmail = True
+        input_2nd_email = ''
+        exists2ndEmail = False     
         while cntEmail <= 2:
-            input_sEmail = input(sEmail + ' #{}: '.format(cntEmail))
-            if cntEmail == 1:
-                input_1st_email = input_sEmail        
-            cntEmail = cntEmail + 1
-            input_2nd_email = ''
-            exists2ndEmail = False
-            if input_sEmail != '' and cntEmail == 3:
-                input_sEmailType = input(sEmailType)      
-                input_2nd_email = homeVSwork(input_sEmailType,input_sEmail,sEmailType)
+            if canEnterAdditionalEmail is True:
+                input_sEmail = input(sEmail + ' #{}: '.format(cntEmail))
+            input_1st_email = ''
+            if cntEmail == 1 and input_sEmail != '':
+                input_1st_email = input_sEmail
+                exists1stEmail = True
+            elif cntEmail == 1 and input_sEmail == '':
+                exists1stEmail = False
+                canEnterAdditionalEmail = False 
+            if exists1stEmail is True and cntEmail == 2:
+                if input_sEmail != '':
+                    input_sEmailType = input(sEmailType)      
+                    input_2nd_email = homeVSwork(input_sEmailType,input_sEmail,sEmailType)
                 if input_2nd_email != '':
-                    input_sEmail = str(' #1: ' + input_1st_email + lb + sEmail + ' #2: ' + input_2nd_email)
+                    input_sEmail = (' #1: ' + input_1st_email + lb + sEmail + ' #2: ' + input_2nd_email)
                     exists2ndEmail = True
-            if exists2ndEmail is False or input_2nd_email == '':
+            if exists1stEmail is True and (exists2ndEmail is False or input_2nd_email == ''):
                 input_sEmail = input_1st_email + ':'
+            cntEmail = cntEmail + 1 
         input_sAddress = input(sAddress)
         input_sBirthday = input(sBirthday)
         input_sNickname = input(sNickname)
@@ -71,20 +101,13 @@ def Main():
         fileInput = (sName + input_sName + lb + sPhone + input_sPhone + lb +
             sEmail + input_sEmail + lb + sAddress + input_sAddress + lb +
             sBirthday + input_sBirthday + lb + sNickname + input_sNickname + lb +
-            sOccupation + input_sOccupation + lb + sCompany + input_sCompany + lb)
+            sOccupation + input_sOccupation + lb + sCompany + input_sCompany + lb +
+            '-----------------------------------------------------------' + lb)
         if fileInput != '':
             cntAddition = cntAddition + 1
-            if cntAddition == 1:
-                f = open('AddressBook.txt','w+')
-            else:
-                f = open('AddressBook.txt','a')
-            for i in fileInput:
-                f.write(i)
-            f.close
+            storeInput(filename,fileInput,cntAddition)
         input_sAddMore = input(sAddMore)
     if input_sAddMore == 'N' or input_sAddMore == 'n':
-        r = open('AddressBook.txt','r')
-        content = r.read()
-        r.close()
-        print(content)
+        lb
+        getInput(filename,'r')
 Main()
